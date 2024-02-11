fb077e79cd75275bd07ce1d181e1fb32f826c4ef
toniname <60040991+toniname@users.noreply.github.com>
2024-02-11 22:54:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 
 import static java.lang.System.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int n = 10;
